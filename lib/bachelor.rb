19006bee39634797b85134a6239b9c8c4efdd443
Sabrina Chowdhury <nebula155@gmail.com>
2019-02-26 17:53:45 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,19 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
-end
+name = " "
+  data.collect do |seasons, season_data|
+    if seasons.to_s.include?(season)
+        season_data.collect do |array|
+          array = array.to_a
+            array.collect do |things|
+              if things.include?("Winner")
+              name = array[0][1]
+              end
+          end
+        end
+      end
+    end
+    name.split(" ").first
+  end
 
 def get_contestant_name(data, occupation)
   # code here
