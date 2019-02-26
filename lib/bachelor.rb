4f3c508035c08f143a9bc466ae960e5f021d9e4b
Sabrina Chowdhury <nebula155@gmail.com>
2019-02-26 18:59:06 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,75 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
-end
+name = " "
+  data.collect do |seasons, season_data|
+    if seasons.to_s.include?(season)
+        season_data.collect do |array|
+          array = array.to_a
+            array.collect do |things|
+              if things.include?("Winner")
+              name = array[0][1]
+              end
+          end
+        end
+      end
+    end
+    name.split(" ").first
+  end
 
 def get_contestant_name(data, occupation)
-  # code here
+  name = " "
+  data.collect do |seasons, season_data|
+    season_data.collect do |array|
+      array = array.to_a
+      array.collect do |data|
+        if data.include?(occupation)
+        name = array[0][1]
+        end
+      end
+    end
+  end 
+  name
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.collect do |season, season_data|
+    season_data.collect do |array|
+      array= array.to_a
+        array.collect do |data|
+        if data.include?(hometown)
+        counter += 1
+        end
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.collect do |season, season_data|
+    season_data.collect do |array|
+      array = array.to_a
+      array.collect do |thing|
+        if thing.find {|x| x == hometown}
+        return array[3][1]
+        end
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = []
+  data.collect do |seasons, season_data|
+    if seasons.to_s.include?(season)
+      season_data.collect do |array|
+        array.collect do |things|
+          array = array.to_a
+          ages << array[1][1].to_i
+        end
+      end
+    end
+  end
+  ages = ages.reduce {|sum, el| sum + el }.to_f / ages.size
+  ages.round
 end
